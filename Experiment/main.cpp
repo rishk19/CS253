dc3c89c1cb8c1f8a1d450b2ee0f4117f576c0743
Rishabh Kothary <rishabhkothary76@gmail.com>
2022-03-31 23:36:30 +0530

    change in j.son file

--- Experiment/main.cpp
+++ Experiment/main.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 int main(){
     cout <<"Hello"<< endl;
-    vector <int> alpha(10);
-    // alpha[0] =  1;
-    // alpha.push_back(1);
+    vector <int> alpha;
+    //alpha[0] =  1;
+    alpha.push_back(1);
     cout<<"Hello Back";
 
     return 0;
